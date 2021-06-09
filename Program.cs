fff5a1b2e16349b18e138f0020eabc4297b19a31
Alivia-m <aliviagayle@icloud.com>
2021-06-09 10:47:54 -0500

    first changes

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what are you?"); 
+            
+            var you = Console.ReadLine();
+
+
+            Console.WriteLine($"Hello, {you}");
+
+            Console.WriteLine("What year is it?");
+
+            var year = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Hello {you} in the year of {year}");
+        
+        }
+    }
+}
